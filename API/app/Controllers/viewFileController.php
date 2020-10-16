0c57dd7e15c96a958197ed6e5a955805dc02c16d
egg-staff <rattapol@egg.co.th>
2020-10-16 12:55:47 +0700

    add feature for show image from controller

--- API/app/Controllers/viewFileController.php
+++ API/app/Controllers/viewFileController.php
@@ -19,21 +19,17 @@ class viewFileController extends Controller
 
     public function index()
     {
-        echo ROOTPATH;
-        echo '<br/>';
-        echo SYSTEMPATH;
-        echo '<br/>';
-        echo WRITEPATH;
-        // $filepath = 'C:\xampp\htdocs\test_web_new\test_web_new\API\writable\uploads\1.jpg';
-        // if(file_exists($filepath)){ 
-        //     $mime = mime_content_type($filepath); //<-- detect file type
-        //     header('Content-Length: '.filesize($filepath)); //<-- sends filesize header
-        //     header("Content-Type: $mime"); //<-- send mime-type header
-        //     header('Content-Disposition: inline; filename="'.$filepath.'";'); //<-- sends filename header
-        //     readfile($filepath); //<--reads and outputs the file onto the output buffer
-        //     die(); //<--cleanup
-        //     exit; //and exit
-        //   }
+
+        $filepath = WRITEPATH.'/uploads/item/images.png';
+        if(file_exists($filepath)){ 
+            $mime = mime_content_type($filepath); //<-- detect file type
+            header('Content-Length: '.filesize($filepath)); //<-- sends filesize header
+            header("Content-Type: $mime"); //<-- send mime-type header
+            header('Content-Disposition: inline; filename="'.$filepath.'";'); //<-- sends filename header
+            readfile($filepath); //<--reads and outputs the file onto the output buffer
+            die(); //<--cleanup
+            exit; //and exit
+          }
 
     }
 }
