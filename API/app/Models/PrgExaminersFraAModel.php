313ba46249f4ba80f55e0c1ec04ae1a6c8489625
Rattapol Apipoltanakron <rattapol@egg.co.th>
2021-04-02 17:09:25 +0700

    update model for test

--- API/app/Models/PrgExaminersFraAModel.php
+++ API/app/Models/PrgExaminersFraAModel.php
@@ -10,49 +10,63 @@ class PrgExaminersFraAModel extends Model {
 
     protected $useAutoIncrement = true;
 
-    protected $returnType     = 'array';
+    protected $returnType     = 'object';
     protected $useSoftDeletes = true;
 
-    protected $allowedFields = ['examiner_id', 'exam_date',];
+    protected $allowedFields = [
+        'examiner_id',
+        'exam_date',
+        'question_1',
+        'question_2',
+        'question_3',
+        'question_4',
+        'question_5',
+        'question_6',
+        'question_7',
+        'question_8',
+        'question_9',
+        'question_10',
+        'question_11',
+        'question_12',
+        'question_13',
+        'question_14',
+        'question_15',
+        'question_16',
+        'question_17',
+        'question_18',
+        'question_19',
+        'question_20',
+        'question_21',
+        'question_22',
+        'question_23',
+        'question_24',
+        'question_25',
+        'height',
+        'weight',
+        'bmi',
+        'remark',
+        'active_flag',
+        'create_date',
+        'update_date',
+        'create_user',
+        'update_user'
+    ];
 
-    // protected $useTimestamps = false;
-    // protected $createdField  = 'created_at';
-    // protected $updatedField  = 'updated_at';
-    // protected $deletedField  = 'deleted_at';
+    protected $useTimestamps = true;
+    protected $createdField  = 'create_date';
+    protected $updatedField  = 'update_date';
 
-    // protected $validationRules    = [];
-    // protected $validationMessages = [];
-    // protected $skipValidation     = false;
+    protected $validationRules    = [
+        'examiner_id'     => 'required|integer',
+        'exam_date'       => 'required|valid_date'
+    ];
+    
+    protected $validationMessages = [
+       
+    ];
+    protected $skipValidation     = false;
 
-    // protected $table        = 'prg_examiners_fra_a';
-    // protected $primaryKey   = 'id';
+    protected $protectFields = false;
 
-    // //protected $useAutoIncrement = true;
-
-    // protected $returnType     = 'object';
-    // //protected $protectFields = false;
-    // // protected $useAutoIncrement = true;
-
-    // // protected $returnType     = 'array';
-    // // protected $useSoftDeletes = true;
-
-    // protected $allowedFields = ['name', 'email', 'address'];
-
-    // // protected $useTimestamps = false;
-    // // protected $createdField  = 'created_at';
-    // // protected $updatedField  = 'updated_at';
-    // // protected $deletedField  = 'deleted_at';
-
-    // // protected $validationRules    = [];
-    // // protected $validationMessages = [];
-    // // protected $skipValidation     = false;
-    // //protected $validationRules    = [
-    // //    'examiner_id'     => 'required',
-    // //    'exam_date'       => 'required'
-    // //];
-
-    // //protected $validationMessages = [
-    // //   
-    // //];
 }
 ?>
